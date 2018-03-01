144f6a9f75bda8061ebde0b25a97113c3c0f696b
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-01 16:29:44 +0530

    Multiplication of given three numbers Program

---
+++ QaSession/Multiplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, result;
+            Console.WriteLine("Enter first value to multiply:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number to multiply:");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number to multiply:");
+            num3 = int.Parse(Console.ReadLine());
+            result = num1 * num2 * num3;
+            Console.WriteLine(num1 + "*" + num2 + "*" + num3 + "=" + result);
+            Console.ReadLine();
+
+        }
+    }
+}
