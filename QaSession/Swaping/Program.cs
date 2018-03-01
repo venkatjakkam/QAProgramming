f894b49419521aedca3726f387ca0f3db47197ba
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-01 16:10:52 +0530

    Swapping and Tables programs

---
+++ QaSession/Swaping/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swaping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Enter first number:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number:");
+            num2 = int.Parse(Console.ReadLine());
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+            Console.WriteLine("After swap:");
+            Console.WriteLine("First Number:" + num1);
+            Console.WriteLine("Second Number:" + num2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ QaSession/Tables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i;
+            Console.WriteLine("Enter a number:");
+            num = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+            for (i = 1; i <= 10; i++)
+            {
+                Console.Write("{0} X {1} = {2} \n", num, i, num * i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
