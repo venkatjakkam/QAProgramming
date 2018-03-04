ae448df8e91741c58770e8cd1d1caeedac87de76
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-04 15:54:18 +0530

    Print Oddnumbers

---
+++ QaSession/OddNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddnumbersP5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Odd numbers from 1 to 99");
+            for (i = 1; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    Console.Write("\n");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QaSession/SumofIntegers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum, mul;
+            Console.WriteLine("Enter first number:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            num2 = int.Parse(Console.ReadLine());
+            if (num1 == num2)
+            {
+                mul = 3 * (num1 + num2);
+                Console.WriteLine("Expected output:" + mul);
+                Console.ReadLine();
+            }
+            else
+            {
+                sum = num1 + num2;
+                Console.WriteLine("Expected output:" + sum);
+                Console.ReadLine();
+            }
+        }
+    }
+}
