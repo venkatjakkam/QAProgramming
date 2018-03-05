20f6edf6936173b554492b370c0bfa3c832875be
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-05 14:41:03 +0530

    Check odd numbers in the given array of integers..

---
+++ QaSession/CheckOddNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckOddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 4, 7, 8, 6 };
+            int i;
+            Boolean check = true;
+            Console.Write("\nElements in the array:");
+            for (i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            for (i=0;i<arr.Length;i++)
+            {
+                if(check = arr[i]%2!=0)
+                {
+                    Console.WriteLine("\nCheck if an array contains an odd number?" + check);
+                }
+                
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
