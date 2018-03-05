080c590ad1382656c9e35fdd2ed0256987c0987c
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-05 15:47:37 +0530

    Letters reverse order

---
+++ QaSession/ReverseOrder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char l1, l2, l3;
+            Console.WriteLine("Enter a letter:");
+            l1 = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter a letter:");
+            l2 = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter a letter:");
+            l3 = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("{0}{1}{2}",l3,l2,l1);
+            Console.ReadLine();
+        }
+    }
+}
