f9f267afa42906a55cb78601387fbed7e9aa5980
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-06 23:51:15 +0530

    Triangles

---
+++ QaSession/TrianglesP17/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianglesP17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Test Data:");
+            a = Convert.ToInt32(Console.Read());
+            b = Convert.ToInt32(Console.Read());
+            c = Convert.ToInt32(Console.Read());
+            Console.ReadLine();
+            Console.WriteLine("Expected output:");
+            if(a == b&&a==c&&b==c)
+            {
+                Console.WriteLine("Equilateral ");
+            }
+            else if(a != b && b != c&&a!=c)
+             {
+                Console.WriteLine("Scalene");
+            }
+            else
+            {
+                Console.WriteLine("Isosecles");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
