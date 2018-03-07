7b7332a12cef16c26a49f47d23e77791f2ab23e7
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-07 10:55:24 +0530

    Leap Year

---
+++ QaSession/LeapyearP16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapyearP16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter a year:");
+            year = Convert.ToInt32(Console.ReadLine());
+            if(year%4==0)
+            {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a leap year ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
