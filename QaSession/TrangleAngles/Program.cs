2e0b75632aa76c4c87f1cb99d8b96e324e6e5118
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-07 10:18:41 +0530

    Trangles fromation using angles

---
+++ QaSession/TrangleAngles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrangleAngles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            int d = 0;
+            Console.WriteLine("Enter First angle:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second angle:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Third angle:");
+            c = Convert.ToInt32(Console.ReadLine());
+            d = a + b + c;
+            if(d<180)
+            {
+                Console.WriteLine("This trangle is not valid..");
+            }
+            else
+            {
+                Console.WriteLine("This trangle is valid..");
+            }
+            Console.ReadLine();
+        }
+    }
+}
