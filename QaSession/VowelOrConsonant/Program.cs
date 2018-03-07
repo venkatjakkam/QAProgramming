572c56c304fc1c477cf7630605d10a6b06555fef
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-07 10:41:33 +0530

    Given Alphabet is Vowel or consonant

---
+++ QaSession/VowelOrConsonant/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelOrConsonant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Alphabet:");
+            char ch;
+            ch = Convert.ToChar(Console.ReadLine().ToLower());
+
+            switch (ch)
+            {
+                case 'a':
+                    Console.WriteLine("The alphabet is a Vowel.");
+                    break;
+                case 'e':
+                    Console.WriteLine("The alphabet is a Vowel.");
+                    break;
+                case 'i':
+                    Console.WriteLine("The alphabet is a Vowel.");
+                    break;
+                case 'o':
+                    Console.WriteLine("The alphabet is a Vowel.");
+                    break;
+                case 'u':
+                    Console.WriteLine("The alphabet is a Vowel.");
+                    break;
+                default:
+                    Console.WriteLine("The alphabet is a consonant.");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
