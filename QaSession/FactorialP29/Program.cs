c167cbed13de1e2c4f0198c7ddd839b93356eacc
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-08 12:35:17 +0530

    Factorial of a given number

---
+++ QaSession/FactorialP29/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialP29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, fact;
+            Console.WriteLine("Enter a number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            fact = n;
+            for(i=n-1;i>=1;i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial of given number is:" + fact);
+            Console.ReadLine();
+        }
+    }
+}
