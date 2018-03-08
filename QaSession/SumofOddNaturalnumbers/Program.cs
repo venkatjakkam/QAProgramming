2e48a4e4cd9108040c0a661de48313b5e2aa5dad
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-08 11:07:09 +0530

    Sum of Odd Natural numbers..P24

---
+++ QaSession/SumofOddNaturalnumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumofOddNaturalnumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,n;
+            int sum = 0;
+            Console.WriteLine("Input Number of terms: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i=1;i< (2*n);i+=2)
+            {
+                Console.WriteLine("Odd numbers are:" + i);
+                sum = sum + i;
+            }
+            
+            Console.WriteLine("Sum of first" + n + "Numbers is:" + sum);
+            Console.ReadLine();
+        }
+        
+    }
+}
