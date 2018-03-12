487fea287dc0d7818c2d03b5994dc6b0cf787611
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-12 17:33:38 +0530

    Print Reverse of a given number

---
+++ QaSession/ReverseaNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseaNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, reverse = 0,remainder;
+            Console.WriteLine("Enter a number to reverse:");
+            n = Convert.ToInt32(Console.ReadLine());
+            while(n>0)
+            {
+                remainder = n % 10;
+                reverse = (reverse * 10) + remainder;
+                n = n / 10;
+            }
+
+            Console.WriteLine("Reverse of given number is:" + reverse);
+            Console.ReadLine();
+        }
+    }
+}
