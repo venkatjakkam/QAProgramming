4cb9ef91bb37e5f61176efbce4627ba3880f2e09
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-12 17:42:44 +0530

    Palindrome number

---
+++ QaSession/PalindromeP39/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeP39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, reverse=0, remainder,original;
+            Console.WriteLine("Enter a number to check palindrome or not:");
+            n = Convert.ToInt32(Console.ReadLine());
+            original = n;
+            while (n>0)
+            {
+                remainder = n % 10;
+                reverse = (reverse * 10) + remainder;
+                n = n / 10;
+            }
+            if(original == reverse)
+            {
+                Console.WriteLine(original + " is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine(original + " is not a palindrome");
+            }
+            Console.ReadLine();
+        }
+    }
+}
