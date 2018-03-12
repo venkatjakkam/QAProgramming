4171a17befc3f4dfd46a23e54825f3d786f83d8d
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-12 15:13:48 +0530

    Febonaccai Series for a given range

---
+++ QaSession/Febonacci SeriesP37/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Febonacci_SeriesP37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;
+            int number;
+            Console.WriteLine("Enter a number for Febonacci series limit:");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Febonacci are:{0}", a);
+            Console.WriteLine("Febonacci are:{0}", b);
+            for (int i=0;i<number-2;i++)
+            {  
+                c = a + b;
+                Console.WriteLine("Febonacci are:{0}",c);
+                a = b;
+                b = c;
+            }
+            Console.ReadLine();
+        }
+    }
+}
