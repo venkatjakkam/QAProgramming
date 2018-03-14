e5698f692ba80147b43a7d4a7eef0a2971974c01
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-14 11:02:20 +0530

    Integers Divisible by 9 between 100 to 200..Program#40

---
+++ QaSession/Divisibleby9P40/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divisibleby9P40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, sum = 0;
+            Console.WriteLine("Integers Divisible by 9 are:");
+            for (i=100;i<=200;i++)
+            {
+                if(i%9==0)
+                {
+                    Console.Write(" "+i);
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine("\nThe sum:"+sum);
+            Console.ReadLine();
+        }
+    }
+}
