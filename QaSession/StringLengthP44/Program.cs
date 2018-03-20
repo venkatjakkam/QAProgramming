ff9425a1b80a460dee553f6fa32471c14256f3eb
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-20 16:09:54 +0530

    StringLengthP44

---
+++ QaSession/StringLengthP44/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLengthP44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = " ";
+            int length = 0;
+
+            Console.WriteLine("Enter a String to find lenth:");
+            str = Console.ReadLine();
+            foreach(char ch in str)
+            {
+                length++;
+            }
+            Console.WriteLine("Length is:" + length);
+            Console.ReadLine();
+
+        }
+    }
+}
