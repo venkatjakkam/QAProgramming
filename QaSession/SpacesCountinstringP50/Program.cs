168d04bc6c4b27010eadaef57a53e6b7f52f8ee2
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-28 16:03:02 +0530

    Number of spaces in Given string..Program50

---
+++ QaSession/SpacesCountinstringP50/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacesCountinstringP50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Enter a string:");
+            string str = Console.ReadLine();
+
+            for(int i=0;i<str.Length;i++)
+            {
+                if(str[i]==' ')
+                {
+                    count++;
+                }
+                
+            }
+            Console.WriteLine("Spaces in the string are:" + count);
+            Console.ReadLine();
+        }
+    }
+}
