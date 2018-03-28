3c28a83ddcf4c728207ccf7bab4669672a8a0d00
venkatjakkam <venkateswara.jakkam@campsystems.com>
2018-03-28 10:50:41 +0530

    Substring exists in given stringP48

---
+++ QaSession/SubStringExistsP48/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubStringExistsP48
+{
+    class Program
+    {
+        public void substing()
+        {
+            Console.WriteLine("Input a String: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Input a string search:");
+            string substr = Console.ReadLine();
+            string matchstr = string.Empty;
+           // bool contains = false;
+
+            for(int i=0;i<str.Length;i++)
+            {
+               // matchstr = matchstr+str[i];
+               for(int j= i;j<i+substr.Length;j++)
+                {
+                    if(j<str.Length)
+                    {
+                        matchstr = matchstr + str[j];
+                    }
+                    else
+                    {
+                        //Console.WriteLine("The substring is not present..");
+                        break;
+                    }
+                }
+                //Console.WriteLine(matchstr);
+                if(matchstr==substr)
+                {
+                    Console.WriteLine("Given substring is present");
+                    break;
+                }
+                else
+                {
+                    matchstr = string.Empty;
+                    
+                }
+                
+            }
+            Console.ReadLine();
+
+        }
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.substing();
+        }
+    }
+}
